5556dc45f97f55c97af2dccc93aed7324f2c0fd5
Mikael Alfredsson <mazynoc@gmail.com>
2018-06-01 17:00:29 +0200

    update to jetpack

--- example/src/main/java/nu/annat/simplesqlexample/MainActivity.java
+++ example/src/main/java/nu/annat/simplesqlexample/MainActivity.java
@@ -1,7 +1,7 @@
 package nu.annat.simplesqlexample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
